505337872ed8ed6915e452dc905a5226de4b43ed
Usman4862 <um4862@gmail.com>
2023-03-06 18:43:02 +0500

    add home

--- JobFreedom/urls.py
+++ JobFreedom/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
+
+
+def home(request):
+    return render(request, "skillhunt/index.html")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
