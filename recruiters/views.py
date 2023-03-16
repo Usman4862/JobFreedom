61901500e0e645daadbf0e442031f7631218b4b6
Mubashir12392 <mubashirhaider01@gmail.com>
2023-03-16 14:43:06 +0500

    add some views in recrutiters app

--- recruiters/views.py
+++ recruiters/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from .models import Recruiter
+from .models import Recruiter, JobCategory, JobPost
 # Create your views here.
 
 
 def home(request):
-    return render(request, "index.html")
+    categories = JobCategory.objects.all().order_by("id")
+    jobposts = JobPost.objects.all().order_by("-created_at")
+    context = {
+        "jobcategories" : categories,
+        "header_categories": categories[:5],
+        "jobposts" : jobposts,
+    }
+    return render(request, "index.html", context=context)
 
 def post_job(request):
     return render(request,'new-job-post.html')
